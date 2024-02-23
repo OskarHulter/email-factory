27d089ed453464b796b8e1af871f7f80ed01126d
OskarHulter <osh@trustpilot.com>
2024-02-23 19:59:14 +0100

    move

---
+++ src/inliner/inliner.ts
@@ -0,0 +1,23 @@
+import juice from "juice";
+// https://automattic.github.io/juice/
+const html = "<style>div{color:red;}</style><div/>";
+const options = {};
+const css = "div{color:red;}";
+
+const res = juice(html);
+console.log("🚀 ~ res:", res);
+
+const resContent = juice.inlineContent(html, css, options);
+console.log("🚀 ~ resContent:", resContent);
+
+function callback(err: unknown, html: string) {
+  if (err) {
+    console.error(err);
+  }
+  console.log(html);
+}
+
+const filePath =
+  "./src/templates/emails/responsive/responsive-email-template.html";
+const resFile = juice.juiceFile(filePath, options, callback);
+console.log("🚀 ~ resFile:", resFile);
